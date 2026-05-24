c41400479aaa6f70fc09d5957451d4354b7d05df
Veenda Putri Divo <divoveenda@gmail.com>
2026-05-24 13:27:41 +0700

    idk

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SkillSnap.Client;
+using SkillSnap.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,4 +14,10 @@ builder.Services.AddScoped<AuthService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+var authService = host.Services.GetRequiredService<AuthService>();
+await authService.InitializeAuthStateAsync();
+
+// start the application
+await host.RunAsync();
